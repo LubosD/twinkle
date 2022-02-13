5645e2539add0daa7c65b41961f7e2831b895b84
Frédéric Brière <fbriere@fbriere.net>
2022-02-13 14:39:59 -0500

    Rename `MD5*` variables to `H_*` in t_request::authorize_md5()
    These variables will soon be used with other hash algorithms, so we give
    them a more generic name.  (The notation was inspired by RFC 7616.)

--- src/parser/request.cpp
+++ src/parser/request.cpp
@@ -206,38 +206,38 @@ bool t_request::authorize_md5(const t_digest_challenge &dchlg,
 		A2 = method2str(method, unknown_method) + ":" + uri.encode();
 		A2 += ":";
 		if (body) {
-			digest_t MD5body("md5");
-			MD5body.puts(body->encode().c_str());
-			A2 += std::string(MD5body.str());
+			digest_t H_body("md5");
+			H_body.puts(body->encode().c_str());
+			A2 += std::string(H_body.str());
 		} else {
-			digest_t MD5body("md5");
-			MD5body.puts("");
-			A2 += std::string(MD5body.str());
+			digest_t H_body("md5");
+			H_body.puts("");
+			A2 += std::string(H_body.str());
 		}
 	}
 	// RFC 2716 3.2.2.1
 	// Caculate digest
-	digest_t MD5A1("md5");
-	digest_t MD5A2("md5");
+	digest_t H_A1("md5");
+	digest_t H_A2("md5");
 
-	MD5A1.puts(A1.c_str());
-	MD5A2.puts(A2.c_str());
+	H_A1.puts(A1.c_str());
+	H_A2.puts(A2.c_str());
 
 	std::string x;
 
 	if (cmp_nocase(qop, QOP_AUTH) == 0 || cmp_nocase(qop, QOP_AUTH_INT) == 0) {
-	        x = std::string(MD5A1.str());
+	        x = std::string(H_A1.str());
 		x += ":";
 		x += dchlg.nonce + ":";
 		x += int2str(nc, "%08x") + ":";
 		x += cnonce + ":";
 		x += qop + ":";
-		x += std::string(MD5A2.str());
+		x += std::string(H_A2.str());
 	} else {
-		x = std::string(MD5A1.str());
+		x = std::string(H_A1.str());
 		x += ":";
 		x += dchlg.nonce + ":";
-		x += std::string(MD5A2.str());
+		x += std::string(H_A2.str());
 	}
 
 	digest_t digest("md5");
